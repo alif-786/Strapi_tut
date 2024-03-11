50d83e02e824a6f0936799196b603bc129057198
alif-786 <alipkamate1@gmail.com>
2024-03-11 20:23:48 +0530

    project intialised

---
+++ src/api/restorunt/controllers/restorunt.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * restorunt controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::restorunt.restorunt');
